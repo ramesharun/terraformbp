1cad1525bf796dc893e5ffa60d9cdf998b41665e
ramesharun <ramesh95arun@yahoo.com>
2021-02-21 20:33:11 -0800

    Update main.tf

--- awsinstance/main.tf
+++ awsinstance/main.tf
@@ -17,10 +17,15 @@ variable "nodes" {
   default = "1"
 }
 
+
+variable "ami" {
+  default = "ami-0a36eb8fadc976275"
+}
+
 resource "aws_instance" "machine1" {
     count         = "${var.nodes}"
-    ami           = "ami-0a36eb8fadc976275"
-    instance_type = "t2.micro"
+    ami           = "${var.ami}"
+    instance_type = "${var.instance_type}"
     availability_zone = "us-west-2a"
     tags = {
       "type" = var.myTag
